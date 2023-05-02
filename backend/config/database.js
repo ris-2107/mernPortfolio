793956c3a441624f1051a9fe198e7dfe8c60f9aa
Rishabh Sinha <67827763+ris-2107@users.noreply.github.com>
2023-05-03 04:08:00 +0530

    silly error

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabse = () => {
   mongoose
-    .connect('mongodb+srv://user-portfolio:rishabh123@cluster0.ffesr.mongodb.net/portfolio?retryWrites=tru', {
+    .connect('mongodb+srv://user-portfolio:rishabh123@cluster0.ffesr.mongodb.net/portfolio?retryWrites=true', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
